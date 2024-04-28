91be2c9ca215b05dc6c02415180a08de50bc4369
cares0 <cares00000@gmail.com>
2024-04-28 09:54:13 +0900

    Add annotations for explicit documentation of cookies and headers
    - Add annotations to explicitly document request and response cookies, as well as request and response headers.
    - while @CookieValue and @RequestHeader annotations allow for documentation in cases where requests are resolved through them, it is not possible to document values at compile-time when requests are resolved using Map, MultiValueMap, or HttpHeaders.
    - For response cookies, which cannot be extracted directly from a handler's response, developers can specify the values they wish to document explicitly.

---
+++ core/src/main/kotlin/cares/restdocskdsl/annotation/ResponseCookieDocs.kt
@@ -0,0 +1,15 @@
+package cares.restdocskdsl.annotation
+
+import org.springframework.core.annotation.AliasFor
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ResponseCookieDocs(
+
+    @get: AliasFor("name")
+    val value: Array<String> = [],
+
+    @get: AliasFor("value")
+    val name: Array<String> = [],
+
+)
---
+++ core/src/main/kotlin/cares/restdocskdsl/annotation/ResponseHeaderDocs.kt
@@ -0,0 +1,15 @@
+package cares.restdocskdsl.annotation
+
+import org.springframework.core.annotation.AliasFor
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ResponseHeaderDocs(
+
+    @get: AliasFor("name")
+    val value: Array<String> = [],
+
+    @get: AliasFor("value")
+    val name: Array<String> = [],
+
+)
